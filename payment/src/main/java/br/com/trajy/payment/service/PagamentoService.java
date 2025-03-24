8ceb0596088f0b137304774ccf2ac7b6e13bcf2e
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-23 22:40:04 -0300

    pagamento service

---
+++ payment/src/main/java/br/com/trajy/payment/service/PagamentoService.java
@@ -0,0 +1,25 @@
+package br.com.trajy.payment.service;
+
+import static br.com.trajy.payment.model.constant.MetodoPagamentoType.processarPagamento;
+
+import br.com.trajy.architecture.layer.service.ServiceAbstract;
+import br.com.trajy.payment.model.entity.Pagamento;
+import br.com.trajy.payment.model.entity.PagamentoRealizado;
+import br.com.trajy.payment.repository.PagamentoRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Getter
+public class PagamentoService extends ServiceAbstract<UUID, PagamentoRealizado> {
+
+    private final PagamentoRepository repository;
+
+    public PagamentoRealizado processar(Pagamento pagamento) {
+        return repository.save(processarPagamento(pagamento));
+    }
+
+}
