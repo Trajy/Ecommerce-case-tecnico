96cbf6d29f6a8240ce2b06df172023136a09108b
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-25 00:26:55 -0300

    @Trasactional annotations

--- ecommerce/src/main/java/br/com/trajy/ecommerce/service/EstatisticaService.java
+++ ecommerce/src/main/java/br/com/trajy/ecommerce/service/EstatisticaService.java
@@ -6,6 +6,7 @@ import br.com.trajy.security.model.projection.UsuarioTicketMedioProjection;
 import br.com.trajy.security.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -16,14 +17,17 @@ public class EstatisticaService {
     private final UsuarioRepository usuarioRepository;
     private final PedidoRepository pedidoRepository;
 
+    @Transactional(readOnly = true)
     public List<UsuarioMaisComprouProjection> getUsuariosMaisCompraram(int quantidade) {
         return usuarioRepository.findTopUsuariosMaisCompraram(quantidade);
     }
 
+    @Transactional(readOnly = true)
     public List<UsuarioTicketMedioProjection> getTicketMedio() {
         return usuarioRepository.findTicketMedioPorUsuario();
     }
 
+    @Transactional(readOnly = true)
     public BigDecimal getTotalFaturadoMes() {
         return pedidoRepository.getTotalFaturadoMes();
     }
--- payment/src/main/java/br/com/trajy/payment/service/PagamentoService.java
+++ payment/src/main/java/br/com/trajy/payment/service/PagamentoService.java
@@ -9,6 +9,7 @@ import br.com.trajy.payment.repository.PagamentoRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @Service
@@ -18,6 +19,7 @@ public class PagamentoService extends ServiceAbstract<UUID, PagamentoRealizado> 
 
     private final PagamentoRepository repository;
 
+    @Transactional(rollbackFor = Exception.class)
     public PagamentoRealizado processar(Pagamento pagamento) {
         return repository.save(processarPagamento(pagamento));
     }
