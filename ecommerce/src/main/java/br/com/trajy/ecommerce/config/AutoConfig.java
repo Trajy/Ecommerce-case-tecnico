023d9c3b9e9806affae24e1a0cecb2b8e2f6d7d7
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-25 00:21:31 -0300

    removido OpenApiConfig da classe de autoconfiguracao

--- ecommerce/src/main/java/br/com/trajy/ecommerce/config/AutoConfig.java
+++ ecommerce/src/main/java/br/com/trajy/ecommerce/config/AutoConfig.java
@@ -1,13 +1,11 @@
 package br.com.trajy.ecommerce.config;
 
-import br.com.trajy.architecture.openapi.OpenApiCoreConfig;
 import br.com.trajy.architecture.restful.exception.RestGlobalExecptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({
         RestGlobalExecptionHandler.class,
-        OpenApiCoreConfig.class
 })
 @Configuration
 public class AutoConfig {
