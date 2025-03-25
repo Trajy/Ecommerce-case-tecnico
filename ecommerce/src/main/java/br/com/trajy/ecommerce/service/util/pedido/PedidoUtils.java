90ce5e80837cc6396e85c7020ef689b87965bf92
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-25 00:27:23 -0300

    metodo para tratar erros quando produto nao entrado ao criar pedido

--- ecommerce/src/main/java/br/com/trajy/ecommerce/service/util/pedido/PedidoUtils.java
+++ ecommerce/src/main/java/br/com/trajy/ecommerce/service/util/pedido/PedidoUtils.java
@@ -1,12 +1,18 @@
 package br.com.trajy.ecommerce.service.util.pedido;
 
 import static br.com.trajy.ecommerce.model.constant.PedidoStatusType.CANCELADO;
+import static java.lang.String.format;
 import static java.math.BigDecimal.valueOf;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.apache.commons.collections4.IteratorUtils.find;
+import static org.apache.commons.collections4.IteratorUtils.isEmpty;
 
 import br.com.trajy.ecommerce.model.entity.Pedido;
 import br.com.trajy.ecommerce.model.entity.Produto;
+import br.com.trajy.ecommerce.model.entity.ProdutoPedido;
 import br.com.trajy.ecommerce.model.projection.ProdutoPrecoProjection;
+import javax.persistence.EntityNotFoundException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
@@ -20,6 +26,7 @@ public final class PedidoUtils {
     }
 
     public static void getPrecoAtualProdutos(Pedido pedido, List<ProdutoPrecoProjection> produtoPrecos) {
+        handleNotFoundProdutos(pedido, produtoPrecos);
         pedido.getProdutosPedidos().forEach(produtoPedido ->
             produtoPedido.setPreco(
                 find(produtoPrecos.iterator(), produtoPreco -> produtoPedido.getProduto().getId().equals(produtoPreco.getId())).getPreco()
@@ -30,7 +37,7 @@ public final class PedidoUtils {
     public static void calcularTotalProdutosPedido(Pedido pedido) {
         pedido.setTotalProdutos(
             pedido.getProdutosPedidos().stream()
-                    .map(produtoPedido -> valueOf(produtoPedido.getQuantidade()).multiply(produtoPedido.getProduto().getPreco()))
+                    .map(produtoPedido -> valueOf(produtoPedido.getQuantidade()).multiply(produtoPedido.getPreco()))
                     .reduce(BigDecimal.ZERO, BigDecimal::add)
         );
     }
@@ -45,6 +52,23 @@ public final class PedidoUtils {
             }
             produto.setQuantidadeEstoque(estoqueRestante);
         });
+    }
+
+    private static void handleNotFoundProdutos(Pedido pedido, List<ProdutoPrecoProjection> produtoPrecos) {
+        if(isEmpty(produtoPrecos.iterator())) {
+            throw new EntityNotFoundException("Nenhum produto encontrado para os ids fornecidos.");
+        }
+        List<Produto> produtosNaoEncontrados = pedido.getProdutosPedidos().stream().map(ProdutoPedido::getProduto)
+                .filter(produto -> produtoPrecos.stream().noneMatch(produtoPreco -> produto.getId().equals(produtoPreco.getId())))
+                .toList();
+        if(isNotEmpty(produtosNaoEncontrados)) {
+            throw new EntityNotFoundException(
+                format(
+                    "não encontrados produtos para os ids: %s.",
+                    produtosNaoEncontrados.stream().map(produto -> produto.getId().toString()).collect(joining(", "))
+                )
+            );
+        }
     }
 
 
